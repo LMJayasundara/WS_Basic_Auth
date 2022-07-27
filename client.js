e15182eb118335753857a3cea5b4a0b06b62ef86
LMJayasundara <47645312+LMJayasundara@users.noreply.github.com>
2022-07-27 12:29:16 +0530

    V1.1

--- client.js
+++ client.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 const username = "ID_0001";
 const BasicAuthPassword = "pa$$word";
-const URL = "ws://127.0.0.1:5000/ocpp";
+const URL = "ws://127.0.0.1:8080/ocpp";
 var reconn = null;
 
 function startWebsocket() {
@@ -34,3 +34,34 @@ function startWebsocket() {
 }
 
 startWebsocket();
+
+////////////////////////////////////////////////////////
+
+// var WebSocket = require('ws-reconnect');
+
+// var wsclient = new WebSocket(URL + "" + username,{
+//     retryCount: null, // default is 2
+//     reconnectInterval: 5, // default is 5
+//     perMessageDeflate: false,
+//     headers: {
+//         Authorization: 'Basic ' + Buffer.from(username + ':' + BasicAuthPassword).toString('base64'),
+//     },
+// });
+
+// wsclient.start();
+
+// wsclient.on("message",function(data){
+//     console.log(data);
+// });
+
+// wsclient.on("reconnect",function(){
+//     console.log("reconnecting");
+// });
+
+// wsclient.on("connect",function(){
+//     console.log("connected");
+// });
+
+// wsclient.on("destroyed",function(){
+//     console.log("destroyed");
+// });
--- server.js
+++ server.js
@@ -1,11 +1,33 @@
 const WebSocketServer = require('ws').Server;
 const fs = require('fs');
-const PORT = 5000;
+const PORT = 8080;
 var passwd = 'pa$$word' // should be get form db
 
-const wss = new WebSocketServer({
-    port: PORT,
+var http = require('http');
+var bodyparser = require('body-parser');
+var express = require('express');
+const { resolve } = require('path');
+const { rejects } = require('assert');
+var app = express();
+
+app.use(bodyparser.json());
+
+var server = new http.createServer({
+}, app);
+
+var wss = new WebSocketServer({
+    server,
+    // verifyClient: (info, cb) => {
+    //     // console.log(info.req.client);
+    //     var success = !!info.req.client.authorized;
+    //     console.log(success);
+    //     return success;
+    // }
+
     verifyClient: function (info, cb) {
+        var success = !!info.req.client.authorized;
+        console.log(success);
+
         var authentication = Buffer.from(info.req.headers.authorization.replace(/Basic/, '').trim(),'base64').toString('utf-8');
         if (!authentication)
             cb(false, 401, 'Authorization Required');
@@ -20,9 +42,29 @@ const wss = new WebSocketServer({
                 cb(true, 200, 'Authorized');
             }
         }
-    },
-    rejectUnauthorized: false
+    }
 });
+
+// const wss = new WebSocketServer({
+//     port: PORT,
+//     verifyClient: function (info, cb) {
+//         var authentication = Buffer.from(info.req.headers.authorization.replace(/Basic/, '').trim(),'base64').toString('utf-8');
+//         if (!authentication)
+//             cb(false, 401, 'Authorization Required');
+//         else {
+//             var loginInfo = authentication.trim().split(':');
+//             if (loginInfo[1] != passwd) {
+//                 console.log("ERROR Username / Password NOT matched");
+//                 cb(false, 401, 'Authorization Required');
+//             } else {
+//                 console.log("Username / Password matched");
+//                 info.req.identity = loginInfo[0];
+//                 cb(true, 200, 'Authorized');
+//             }
+//         }
+//     },
+//     rejectUnauthorized: false
+// });
 
 wss.on('connection', function (ws, request) {
     ws.id = request.identity;
@@ -44,4 +86,44 @@ wss.on('connection', function (ws, request) {
 
 });
 
-console.log( (new Date()) + " Server is listening on port " + PORT);
+var check = function(clients, id){
+    return new Promise(function(resolve, rejects){
+        if(clients.size != 0){
+            clients.forEach(function (client) {
+                if(client.id == id){
+                    client.send(id + " Disconnected");
+                    client.close();
+                    resolve(true);
+                }
+                else{
+                    resolve(false);
+                }
+            });
+        }
+        else{
+            resolve(false)
+        }
+    });
+};
+
+server.listen(PORT, ()=>{
+
+    app.post('/test/', function(req, res) {
+        check(wss.clients, req.body.name).then(function(ack) {
+            if(ack){
+                res.json({
+                    success: "true",
+                    result: req.body.name + " Client disconnected"
+                });
+            }
+            else{
+                res.json({
+                    success: "fasle",
+                    result: req.body.name + " Client can not disconnect"
+                });
+            }
+        });
+    });
+
+    console.log( (new Date()) + " Server is listening on port " + PORT);
+});
